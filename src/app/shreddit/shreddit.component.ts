ce75964ebe03038eea728908bdc52f690d4c1f0f
Dominic Brown and Nick Powell <dominic+nick@>
2017-06-12 15:34:16 -0700

    add service

--- src/app/shreddit/shreddit.component.ts
+++ src/app/shreddit/shreddit.component.ts
@@ -1,26 +1,27 @@
-import { Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Post } from './../post.model';
+import { PostService } from './../post.service';
 
 @Component({
   selector: 'app-shreddit',
   templateUrl: './shreddit.component.html',
-  styleUrls: ['./shreddit.component.css']
+  styleUrls: ['./shreddit.component.css'],
+  providers: [PostService]
 })
-export class ShredditComponent {
 
-  constructor(private router: Router){}
 
-  masterPostList: Post[] = [new Post("25 stairs!!!!","Can he do it?","http://afterskate.com/wp-content/uploads/2016/01/jaws-25.jpg",1,1),
-                           new Post("Rodney Mullen","Casper Slide","https://coresites-cdn.factorymedia.com/sidewalk/wp-content/uploads/2015/11/Rodney-Mullen-casper-slide-tre-flip-out.jpg",0,2),
-                           new Post("25 stairs!!!!","Can he do it?","http://afterskate.com/wp-content/uploads/2016/01/jaws-25.jpg",1,3),
-                           new Post("Rodney Mullen","Casper Slide","https://coresites-cdn.factorymedia.com/sidewalk/wp-content/uploads/2015/11/Rodney-Mullen-casper-slide-tre-flip-out.jpg",0,4),
-                           new Post("25 stairs!!!!","Can he do it?","http://afterskate.com/wp-content/uploads/2016/01/jaws-25.jpg",1,5),
-                           new Post("Rodney Mullen","Casper Slide","https://coresites-cdn.factorymedia.com/sidewalk/wp-content/uploads/2015/11/Rodney-Mullen-casper-slide-tre-flip-out.jpg",0,6)
-                           ];
+export class ShredditComponent implements OnInit {
+  posts: Post[];
+
+  constructor(private router: Router, private postService: PostService){}
+
+  ngOnInit() {
+    this.posts = this.postService.getPosts();
+  }
 
   goToDetailPage(clickedPost: Post) {
     this.router.navigate(['posts', clickedPost.id]);
-  };
+  }
 
 }
